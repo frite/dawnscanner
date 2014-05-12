7d5f5df6b572f39f54bfada292a976a5cd4e4aa4
Paolo Perego <thesp0nge@gmail.com>
2014-05-12 18:30:41 +0200

    Dev version bump

--- lib/codesake/dawn/version.rb
+++ lib/codesake/dawn/version.rb
@@ -1,9 +1,9 @@
 module Codesake
   module Dawn
-    VERSION = "1.1.g680ef83"
+    VERSION = "1.1.g820e30f"
     CODENAME = "Lightning McQueen"
     RELEASE = "(development)"
-    BUILD = "30"
-    COMMIT = "g680ef83"
+    BUILD = "37"
+    COMMIT = "g820e30f"
   end
 end
