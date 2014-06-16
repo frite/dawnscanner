70239e917c5e36d8269c78678a3f3cd7c29d3c2c
Paolo Perego <thesp0nge@gmail.com>
2014-06-16 18:44:41 +0200

    Now sinks are detected in a var = Class.method(sink1..n) call.

--- lib/codesake/dawn/core/source.rb
+++ lib/codesake/dawn/core/source.rb
@@ -40,6 +40,10 @@ module Codesake
 
         end
 
+        def stats
+          $logger.log "File: #{File.basename(@filename)} - total lines: #{@total_lines} (empty lines: #{@empty_lines} + comments: #{@comment_lines}); cyclomatic complexity: #{@cyclomatic_complexity}"
+        end
+
         def ast
           (@debug)? @ast : nil
         end
@@ -54,19 +58,13 @@ module Codesake
               sink[:target]   = assign_root[0]
               sink[:filename] = @filename
               sink[:line]     = sexp.line
-              debug_me assign_root[1].class
-              debug_me assign_root[1].sexp_type
               # Trying to understand a call like var = a_method_here(params[1], params[2], ...)
               if assign_root[1].respond_to?(:sexp_type) && assign_root[1].sexp_type.to_sym == :call
-                debug_me "here"
                 sink[:type] = :call
                 sink[:sources_count] = 0
                 sink[:sources] = []
                 assign_root[1].each_sexp do |call_element|
-                  debug_me "#{call_element}"
-                  if call_element.flatten.include?(':params')
-                    debug_me "here"
-
+                  if call_element.flatten.include?(:params)
                     sink[:sources_count] += 1
                     sink[:sources] << call_element.sexp_body.last.sexp_body.sexp_type
                   end
